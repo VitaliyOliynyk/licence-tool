1a8344a435a6195c52c329bb8775e18c8278b04a
Vitaliy Oliynyk <xaoc00@gmail.com>
2012-03-02 17:30:32 +0100

    InitDataLoader (Person, PersonProfile)

--- src/groovy/eu/vitaliy/licencetool/InitDataLoader.groovy
+++ src/groovy/eu/vitaliy/licencetool/InitDataLoader.groovy
@@ -9,12 +9,25 @@ package eu.vitaliy.licencetool
  */
 class InitDataLoader {
 
+    
+    
     void loadData(){
         def eventTypes = createOrGetEventTypes()
+        def profiles = createOrGetPersonProfiles()
+        def persons = createOrGetPerson(profiles[0])
     }
 
-    private createOrGetEventTypes() {
-        def eventTypes = createOrGetData( [
+    private Collection createOrGetPerson(PersonProfile personProfile) {
+
+        createOrGetData([new Person(userName: "user1", password: "123456", profile: personProfile)])
+    }
+
+    private Collection createOrGetPersonProfiles() {
+        createOrGetData([new PersonProfile(firstName: "Jan", lastName: "Kowalski", email: "aaaa@pl.pl")])
+    }
+
+    private Collection createOrGetEventTypes() {
+        createOrGetData( [
                 new EventType(name: "Warszawa JUG"),
                 new EventType(name: "Agile Warsaw"),
                 new EventType(name: "Warszawa DP")
@@ -22,7 +35,7 @@ class InitDataLoader {
     }
 
 
-    private  createOrGetData(domains){
+    private  Collection createOrGetData(domains){
         if(!domains){ return [] }
 
         def DomainType = domains[0].class
--- test/integration/eu/vitaliy/licencetool/InitDataLoaderIntegrationTests.groovy
+++ test/integration/eu/vitaliy/licencetool/InitDataLoaderIntegrationTests.groovy
@@ -6,8 +6,8 @@ import static org.fest.assertions.Assertions.*
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
 
-@Mock(EventType)
-class InitDataLoaderTests {
+@Mock([EventType, PersonProfile, Person])
+class InitDataLoaderIntegrationTests {
 
     @Before
     void setUp() {
@@ -19,10 +19,16 @@ class InitDataLoaderTests {
         // Tear down logic here
     }
 
+    private checkDomainClasses(domainClasses){
+        domainClasses.each {
+            assert it.count() > 0
+        }
+    }
+
     @Test
     void testLoadData() {
         def initDataLoader = new InitDataLoader()
         initDataLoader.loadData()
-        assert EventType.count() > 0
+        checkDomainClasses([EventType, PersonProfile, Person])
     }
 }
